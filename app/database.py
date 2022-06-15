0d2396cd1691ad9395f62f952a5543e9f9b2a324
Alek <alek@spectra-alarm.com>
2022-06-15 13:46:19 +0200

    added basic user model

---
+++ app/database.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from .models import User, db
+from flask import current_app
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def get_all(model):
+    data = model.query.all()
+    return data
+
+
+def add_or_update(instance: db.Model):
+    ret = db.session.merge(instance)
+    commit_changes()
+    return ret
+
+
+def delete_instance(model, id):
+    model.query.filter_by(id=id).delete()
+    commit_changes()
+
+
+def edit_instance(model, id, fields: dict):
+    instance = model.query.filter_by(id=id).first()
+    for attr, new_value in fields.items():
+        setattr(instance, attr, new_value)
+    commit_changes()
+
+
+def find_by_username(username: str) -> Optional[User]:
+    """Finds User by username. If User object is not found, None is returned."""
+    return User.query.filter_by(username=username).first()
+
+
+def find_by_id(model, id):
+    return model.query.get(id)
+
+
+def commit_changes():
+    try:
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        current_app.logger.error(e)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify, request
 
+from app import database
+from app.models import User
+
 api = Blueprint('api', __name__)
 import app.routes_utils
 
 @api.get('/test')
 def test():
-    return jsonify('hello-world')
+    user = User(username='pera',password='pera', id=1)
+    database.add_or_update(user)
+    user = database.find_by_username('pera')
+    print(user.role.name)
+    return jsonify(user.to_dict())

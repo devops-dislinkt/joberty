aa53ba05f696a04789794d21fa23b2cf7ca5fa3d
specnazm <laketic.milena98@gmail.com>
2022-06-25 13:22:00 +0200

    fix: added provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
+provider "heroku" {
+}
+
+
 resource "heroku_app" "servers" {
   name   = "servers-terraform"
   region = "eu"
   stack  = "container"
 }
 
-provider "heroku" {
-}
 
 resource "heroku_addon" "database" {
   app_id  = heroku_app.servers.id

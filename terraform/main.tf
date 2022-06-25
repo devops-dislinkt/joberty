021206945d76fcf0b3bc9dd307553edf1b66d32a
specnazm <laketic.milena98@gmail.com>
2022-06-25 13:15:23 +0200

    ci: use terraform

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "heroku_app" "servers" {
+  name   = "servers-terraform"
+  region = "eu"
+  stack  = "container"
+}
+
+
+
+resource "heroku_addon" "database" {
+  app_id  = heroku_app.servers.id
+  plan = "heroku-postgresql:hobby-dev"
+}

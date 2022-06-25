5331fb155ddc4aceec41ff0f06c922988372c548
specnazm <laketic.milena98@gmail.com>
2022-06-25 14:32:53 +0200

    ci: deploy to heroku

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,13 @@ provider "heroku" {
   # Configuration options
 }
 
+resource "heroku_app" "servers" {
+  name   =  "name"
+  region = "eu"
+  stack  = "container"
+}
+
+
 
 resource "heroku_build" "servers" {
   app_id = heroku_app.servers.id
